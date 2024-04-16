b3977889057b77cc96ddba8add06d335c882878f
Руслан <ruslanstegancev175@gmail>
2024-04-17 00:33:03 +0300

    comments code

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ from sqlalchemy.orm import sessionmaker
 from models import Task, Base
 
 engine = create_engine('sqlite:///test.db')
+# Здесь создается движок (engine) для работы с базой данных SQLite.
+# Он связывается с файлом базы данных test.db.
 
 session_maker = sessionmaker(bind=engine)
+# Тут создается объект session_maker,
+# который используется для создания сессий SQLAlchemy.
+# В данном случае он связывается с созданным ранее движком.
 
 with session_maker() as session:
     Base.metadata.create_all(engine)
+# Этот блок создает сессию базы данных.
+# И автоматически закрывает ее после выполнения всех операций внутри блока.
+# Метод create_all, создает все таблицы,
 
 task1 = Task(
     id=11,
@@ -40,19 +48,37 @@ task5 = Task(
     description="Спать не более 8-9 часов",
     priority=3
 )
+# Создаются объекты задач с различными параметрами.
 
 session.add_all([task1, task2, task3, task4, task5])
 session.commit()
 
+# Здесь создаются экземпляры объектов задач,
+# (task1, task2, task3, task4, task5),
+# с различными значениями id, названия, описания и приоритета.
+# Затем эти задачи добавляются в сессию
+# и фиксируются в базе данных с помощью метода commit.
 
 priority_input = int(
     input("Введите число приоритета для просмотра задачи от 1 до 3: "))
+# ТУт строка запрашивает у пользователя ввод числа приоритета,
+# для отображения соответствующих задач.
 
 with session_maker() as session:
     tasks = session.query(Task).filter(
         Task.priority == str(priority_input)).all()
 
+    # Здесь создается новая сессия,
+    # в которой выполняется запрос к базе данных.
+    # Из базы данных извлекаются все задачи с приоритетом,
+    # соответствующим введенному пользователем значению.
+
     for task in tasks:
         print("\n"
               f"Задача id={task.id}\n{task.title}\n{task.description}\n"
               f"Приоритет - {task.priority}")
+
+# После получения задач с указанным приоритетом они выводятся на экран,
+# с помощью цикла for.
+# Каждая задача выводится в отдельной строке в формате,
+# указанном в строке форматирования.
